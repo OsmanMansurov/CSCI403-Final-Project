93d52cba77f3144e9c1e938fad9d3a3799d823ff
Chores <Chores@Omars-MacBook-Air-2.local>
2025-04-27 13:45:29 -0600

    altered sunburts slightly

--- sunburst_diagram.py
+++ sunburst_diagram.py
@@ -19,10 +19,9 @@ def makeColorColumn(gdf, variable, vmin, vmax):
 
 gdf = gpd.read_file('data/wa_sl_2012_to_2021')
 
-#username = input("Enter username for database access: ")
-username = "omansurov"
-#password = getpass()
-password = "Padajdee123!@#"
+username = input("Enter username for database access: ")
+password = getpass()
+
 connection = pg8000.connect(user=username, password=password, host="ada.mines.edu", port=5432, database="csci403")
 
 cursor = connection.cursor()
@@ -57,30 +56,11 @@ fig = px.sunburst(
 )
 fig.show()
 '''
-heatmap_data = gdf.merge(car_counts, left_on='OBJECTID',right_on='district')
-
-variable = 'car_counts'
-vmin = math.floor(heatmap_data[variable].min()/1000)*1000
-vmax = math.ceil(heatmap_data[variable].max()/1000)*1000
-heatmap_data = makeColorColumn(heatmap_data, variable, vmin, vmax)
-heatmap_data_vis = heatmap_data.to_crs({'init':'epsg:3857'})
-
-fig, ax = plt.subplots(1, figsize=(8, 4.5))
-ax.axis('off')
-fig = ax.get_figure()
-cbax = fig.add_axes([0.87, 0.05, 0.03, 0.6])
-cbax.set_title('No. Electrical\nVehicles', fontdict={'fontsize': '9', 'fontweight' : '0'})
-cbax.tick_params(labelsize=7)
-
-sm = plt.cm.ScalarMappable(cmap="YlGnBu", norm=plt.Normalize(vmin=vmin, vmax=vmax))
-fig.colorbar(sm, cax=cbax)
-
-for row in heatmap_data_vis.itertuples():
-    vf = heatmap_data_vis[heatmap_data_vis["district"]==row.OBJECTID]
-    c = heatmap_data[heatmap_data["district"]==row.OBJECTID][0:1].value_determined_color.item()
-    vf.plot(color=c, edgecolors='k', linewidth=0.8, ax=ax)
-
-plt.show()
+UPDATE vehicle_details SET "Electric Vehicle Type" = 'Battery EV' WHERE "Electric Vehicle Type" = 'Battery Electric Vehicle (BEV)';
+UPDATE 2209
+csci403=> SELECT * FROM vehicle_details;
+csci403=> UPDATE vehicle_details SET "Electric Vehicle Type" = 'Hybrid EV' WHERE "Electric Vehicle Type" = 'Plug-in Hybrid Electric Vehicle (PHEV)';
+UPDATE 5340
 '''
 connection.commit()
 cursor.close()
