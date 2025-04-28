41592699770ca752b8196befb773099bb335a943
Aidan Morganfield <acmorganfield@isengard.mines.edu>
2025-04-27 21:05:31 -0600

    Cleaned repo up for submission.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CSCI403-Final-Project
+Aidan Morganfield, Chris Olsten, Osman Mansurov, Omar Mansurov
+Professor Fierro
+CSCI403 Database Management
+04/28/2025
--- project.sql
+++ project.sql
@@ -1,3 +1,11 @@
+/*
+CSCI403-Final-Project
+Aidan Morganfield, Chris Olsten, Osman Mansurov, Omar Mansurov
+Professor Fierro
+CSCI403 Database Management
+04/28/2025
+*/
+
 SET search_path TO group12;
 SET ROLE group12;
 
@@ -9,7 +17,11 @@ DROP TABLE if exists vehicle_details CASCADE;
 DROP TABLE if exists vehicle_location CASCADE;
 
 
--- Initial Schemas (CREATE TABLE ...)
+/*
+------------------------------------------------------------------------------------------
+Electric Vehicle Data
+------------------------------------------------------------------------------------------
+*/
 
 CREATE TABLE electric_vehicles (
     "VIN (1-10)" TEXT,
@@ -31,7 +43,10 @@ CREATE TABLE electric_vehicles (
 );
 \copy electric_vehicles FROM 'data/Electric Vehicle Data.csv' WITH (DELIMITER ',', FORMAT CSV, HEADER);
 
---Data cleaning
+--Data Cleaning (Deliverable 1 -- Part 1/2)
+--This work was done by Chris Olsten for Take Home Assignment 2
+--It was included as part of the data cleaning deliverable as the cleaning was important for joining with the additional data we collected
+--The new data set also required a significant amount of additional cleaning
 UPDATE electric_vehicles
 SET "Make" = NULL WHERE TRIM("Make") = '';
 
@@ -46,8 +61,8 @@ WHERE "Electric Range"::TEXT ~ '^[0-9]+$';
 DELETE FROM electric_vehicles
 WHERE "Electric Range" = 0;
 
---Normalizing into BCNF
-
+--Schema Design: Normalizing into BCNF and Adding Keys and constraints
+--This work was done by Chris Olsten for Take Home Assignment 2 and was not used as a deliverable for our final project report
 CREATE TABLE vehicle_details (
     "VIN (1-10)" TEXT NOT NULL,
     "Model" TEXT NOT NULL,
@@ -99,9 +114,6 @@ WHERE "VIN (1-10)" IS NOT NULL AND "DOL Vehicle ID" IS NOT NULL AND "Legislative
 DELETE FROM details_location
 WHERE "DOL Vehicle ID" NOT IN (SELECT "DOL Vehicle ID" FROM vehicle_location);
 
-
--- Keys and constraints
-
 ALTER TABLE vehicle_details ADD CONSTRAINT pk_vehicle_details PRIMARY KEY ("VIN (1-10)");
 ALTER TABLE vehicle_location ADD CONSTRAINT pk_vehicle_location PRIMARY KEY ("DOL Vehicle ID");
 ALTER TABLE details_location ADD CONSTRAINT pk_details_location PRIMARY KEY ("VIN (1-10)", "DOL Vehicle ID");
@@ -112,66 +124,12 @@ ALTER TABLE details_location ADD CONSTRAINT fk_details_location_dol FOREIGN KEY 
 ALTER TABLE vehicle_details ADD CONSTRAINT chk_model_year CHECK ("Model Year" >= 1900 AND "Model Year" <= EXTRACT(YEAR FROM CURRENT_DATE));
 ALTER TABLE vehicle_details ADD CONSTRAINT chk_electric_range CHECK ("Electric Range" >= 0);
 
---District elections
-CREATE TABLE district_elections (
-    District TEXT,
-    Candidate TEXT,
-    Party TEXT,
-    Total_Votes INT,
-    Won TEXT
-);
 
-\copy district_elections FROM 'data/house_candidate.csv' WITH (DELIMITER ',', FORMAT CSV, HEADER);
-
-CREATE TABLE district_politics (
-    District TEXT,
-    Party Text,
-    Total_Votes INT
-);
-
-INSERT INTO district_politics 
-SELECT district, party, total_votes FROM district_elections WHERE won LIKE 'True';
-
---Term-116 data (I'm not sure if this data is useful)
-DROP TABLE IF EXISTS term_data;
-CREATE TABLE term_data (
-    "id" TEXT,
-    "name" TEXT,
-    "sort_name" TEXT,
-    "email" TEXT,
-    "twitter" TEXT,
-    "facebook" TEXT,
-    "group" TEXT,
-    "group_id" TEXT,
-    "area_id" TEXT,
-    "area" TEXT,
-    "chamber" TEXT,
-    "term" TEXT,
-    "start_date" TEXT,
-    "end_date" TEXT,
-    "image" TEXT,
-    "gender" TEXT,
-    "wikidata" TEXT,
-    "wikidata_group" TEXT,
-    "wikidata_area" TEXT
-);
-\copy term_data FROM 'data/term-116.csv' WITH (DELIMITER ',', FORMAT CSV, HEADER);
-
-DROP TABLE IF EXISTS district_data;
-CREATE TABLE district_data(
-    "group_id" TEXT,
-    "State" TEXT,
-    "Legislative District" INTEGER
-);
-INSERT INTO district_data (group_id, "State", "Legislative District")
-SELECT
-    group_id,
-    SUBSTR(area_id, 1, 2),
-    CAST(SUBSTR(area_id, 4) AS INTEGER)
-FROM term_data;
-
-
---Legislative district information Washington
+/*
+------------------------------------------------------------------------------------------
+20201103_legislative (Washington Legislative District Election Results)
+------------------------------------------------------------------------------------------
+*/
 DROP TABLE IF EXISTS uncleaned_legislative_data;
 CREATE TABLE uncleaned_legislative_data(
     "Race" TEXT,
@@ -183,7 +141,7 @@ CREATE TABLE uncleaned_legislative_data(
 );
 \copy uncleaned_legislative_data FROM 'data/20201103_legislative.csv' WITH (DELIMITER ',', FORMAT CSV, HEADER);
 
-
+--Data Cleaning (Deliverable 1 -- Part 2/2)
 UPDATE uncleaned_legislative_data
 SET "Party" = 'Republican'
 WHERE "Party" LIKE '%Republican%' OR "Party" LIKE '%GOP%';
@@ -217,30 +175,27 @@ WHERE Rank = 1 ORDER BY district;
 
 
 
---Interesting queries(Aidan)
+/*
+------------------------------------------------------------------------------------------
+Interesting Queries (Deliverable 3)
+------------------------------------------------------------------------------------------
+*/
+--Query 1
 SELECT DISTINCT "Make", "Model", AVG("Electric Range") as "Avg_Range"
 FROM vehicle_details
 GROUP BY "Make", "Model"
 ORDER BY "Make", "Model";
 
---Query2
-SELECT COUNT(*) AS vehicles_registered, "Make", "Model"
-FROM vehicle_details
-GROUP BY "Make", "Model"
-ORDER BY vehicles_registered DESC;
+--Query 2
+SELECT party, COUNT(*) from legislative_data
+GROUP by party;
 
 --Query 3
-SELECT COUNT(*) AS vehicles_registered, "Make"
-FROM vehicle_details
-GROUP BY "Make"
-ORDER BY vehicles_registered DESC;
-
---Query 4
 SELECT party, COUNT(*)
 FROM legislative_data JOIN vehicle_location ON legislative_data.district = vehicle_location."Legislative District"
 GROUP BY party;  
 
---Query 5
+--Query 4
 WITH vehicles_registered AS(
     SELECT "Make", "Legislative District"
     FROM ((vehicle_details JOIN details_location ON vehicle_details."VIN (1-10)"=details_location."VIN (1-10)") 
@@ -249,65 +204,3 @@ WITH vehicles_registered AS(
 SELECT "Make", COUNT(*), party
 FROM legislative_data JOIN vehicles_registered ON legislative_data.district = vehicles_registered."Legislative District"
 GROUP BY "Make",party ORDER BY "Make", count DESC;
-
-
---Everything below this point was part of Chris's original assignment.
---We can use some of it so I don't want to delete it, but I am commenting it out for organizational purposes
-/*
--- (Q7) Interesting Queries (SELECT ...)
-
--- Query 1: Find the top 5 most common electric vehicle models
-SELECT "Model", COUNT(*) AS "Total Count"
-FROM vehicle_details
-GROUP BY "Model"
-ORDER BY "Total Count" DESC
-LIMIT 5;
-
--- Query 2: Find the average electric range per state for vehicles with a valid electric range
-SELECT "State", AVG("Electric Range") AS "Average Range"
-FROM vehicle_location
-JOIN details_location ON vehicle_location."DOL Vehicle ID" = details_location."DOL Vehicle ID"
-JOIN vehicle_details ON details_location."VIN (1-10)" = vehicle_details."VIN (1-10)"
-WHERE "Electric Range" IS NOT NULL
-GROUP BY "State"
-ORDER BY "Average Range" DESC;
-
-
--- Query 3: Find the most popular electric vehicle make in each state
-WITH RankedMakes AS (
-    SELECT 
-        vehicle_location."State",
-        vehicle_details."Make",
-        COUNT(*) AS "Total Count",
-        RANK() OVER (PARTITION BY vehicle_location."State" ORDER BY COUNT(*) DESC) AS rank
-    FROM vehicle_location
-    JOIN details_location ON vehicle_location."DOL Vehicle ID" = details_location."DOL Vehicle ID"
-    JOIN vehicle_details ON details_location."VIN (1-10)" = vehicle_details."VIN (1-10)"
-    GROUP BY vehicle_location."State", vehicle_details."Make"
-)
-SELECT "State", "Make", "Total Count"
-FROM RankedMakes
-WHERE rank = 1;
-
--- (Q8) Indexes and performance tuning (CREATE INDEX ...)
-
-CREATE INDEX idx_model ON vehicle_details("Model");
-
-EXPLAIN ANALYZE
-SELECT "Model", COUNT(*) AS "Total Count"
-FROM vehicle_details
-GROUP BY "Model"
-ORDER BY "Total Count" DESC
-LIMIT 5;
-
-CREATE INDEX idx_location_state ON vehicle_location("DOL Vehicle ID", "State");
-CREATE INDEX idx_vehicle_range ON vehicle_details("VIN (1-10)", "Electric Range");
-
-EXPLAIN ANALYZE
-SELECT "State", AVG("Electric Range") AS "Average Range"
-FROM details_location
-JOIN vehicle_location ON vehicle_location."DOL Vehicle ID" = details_location."DOL Vehicle ID"
-JOIN vehicle_details ON details_location."VIN (1-10)" = vehicle_details."VIN (1-10)"
-WHERE "Electric Range" IS NOT NULL
-GROUP BY "State";
-*/
